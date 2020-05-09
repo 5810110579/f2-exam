931d8eb47286ad7ec21d0d649c8457bf29cb79f0
5810110579 <thanatornchua@gmail.com>
2020-05-09 13:05:03 +0700

    13.00 Commit

--- Main.java
+++ Main.java
@@ -6,28 +6,28 @@ import javax.swing.JPanel;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class Main{
     public static void main(String[] args){
-        // try {
-        //     File myfile = new File("test.txt");
-        //     Scanner myReader = new Scanner(myfile);
-        //     while (myReader.hasNextLine()) {
-        //       String data = myReader.nextLine();
+        try {
+            File myfile = new File("test.txt");
+            Scanner myReader = new Scanner(myfile);
+            while (myReader.hasNextLine()) {
+              String data = myReader.nextLine();
 
-        //       System.out.println(data);
-        //     }
-        //     myReader.close();
-        //   } catch (FileNotFoundException e) {
-        //     System.out.println("An error occurred.");
-        //     e.printStackTrace();
-        //   }
+              System.out.println(data);
+            }
+            myReader.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
 
         MyFrame frame = new MyFrame("My App");
-        frame.setSize(400,200);
+        frame.setSize(700,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
@@ -45,27 +45,56 @@ public class Main{
     }*/
 }
 class MyFrame extends JFrame{
+
     private JLabel label;
 
     public MyFrame(String title){
         super(title);
 
-        this.getContentPane().setLayout(new BorderLayout());
+        GridLayout layout = new GridLayout(0,3);
+        layout.setHgap(5);
+        layout.setVgap(2);
+
+        this.getContentPane().setLayout(layout);
+
+        label = new JLabel("Add Key to ZoomFactor");
+        label.setFont(label.getFont().deriveFont(18.0f));
+        this.add(label, BorderLayout.PAGE_START);
 
         JButton button_1 = new JButton("Shape2D");
+        button_1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                System.out.pringln("Name :" + Circle.getName() + "Area :" + Circle.getArea());
+                System.out.pringln("Name :" + Rectangle.getName() + "Area :" + Rectangle.getArea());
+            }
+        });
+
         JButton button_2 = new JButton("Shape3D");
+        button_2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                System.out.pringln("Name :" + Sphere.getName() + "Area :" + Sphere.getArea() + "Volume :" + Sphere.getVolume());
+                System.out.pringln("Name :" + Cylinder.getName() + "Area :" + Cylinder.getArea() + "Volume :" + Cylinder.getVolume());
+                System.out.pringln("Name :" + Cuboid.getName() + "Area :" + Cuboid.getArea() + "Volume :" + Cuboid.getVolume());
+            }
+        });
+
         JButton button_3 = new JButton("All Shape");
+        button_2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                System.out.pringln("Name :" + Circle.getName() + "Area :" + Circle.getArea());
+                System.out.pringln("Name :" + Rectangle.getName() + "Area :" + Rectangle.getArea());
+                System.out.pringln("Name :" + Sphere.getName() + "Area :" + Sphere.getArea() + "Volume :" + Sphere.getVolume());
+                System.out.pringln("Name :" + Cylinder.getName() + "Area :" + Cylinder.getArea() + "Volume :" + Cylinder.getVolume());
+                System.out.pringln("Name :" + Cuboid.getName() + "Area :" + Cuboid.getArea() + "Volume :" + Cuboid.getVolume());
+            }
+        });
 
-        // button_1.addActionListener(new ActionListener(){
+        this.add(button_1);
+        this.add(button_2);
+        this.add(button_3);
         
-        //     @Override
-        //     public void actionPerformed(ActionEvent e) {
-                
-        //     }
-        // })
 
-        this.add(button_1, BorderLayout.LINE_START);
-        this.add(button_2, BorderLayout.CENTER);
-        this.add(button_3, BorderLayout.LINE_END);
+
+
     }
 }
